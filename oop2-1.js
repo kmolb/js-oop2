a2411b65d8aed9e6df45d6c46686b7e8e2c41261
Kamil Olber <olb.kamil@gmail.com>
2018-11-17 10:17:01 +0100

    zad 3

--- oop2-1.js
+++ oop2-1.js
@@ -1,4 +1,22 @@
-class Name {
-
+class box {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        // cz2
+        this.sumThis= ()=>{
+            return this.x + this.y + this.z;
+        }
+        //
+    }
+ vol(){
+    return this.x * this.y * this.z;
+ }
 }
-console.log(typeof Name)
+const home = new box(2, 4, 23);
+
+console.log(home.vol());
+// mnozenie bokow
+//cz2
+console.log(home.sumThis());
+// zsumowanie bokow
